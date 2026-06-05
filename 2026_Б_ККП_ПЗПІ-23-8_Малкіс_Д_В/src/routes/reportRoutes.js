3d706acaedd109bd740d169dd66c9219444447b2
dmytromalkis-lab <dmytro.malkis@nure.ua>
2026-06-05 18:11:15 +0300

    Delete 2026_Б_ККП_ПЗПІ-23-8_Малкіс_Д_В/src directory

--- 2026_Б_ККП_ПЗПІ-23-8_Малкіс_Д_В/src/routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-router.get('/drivers/pdf', reportController.generateDriversPDF);
-router.get('/vehicles/pdf', reportController.generateVehiclesPDF);
-router.get('/routes/pdf', reportController.generateRoutesPDF);
-
-module.exports = router;
